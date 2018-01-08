0d1beebc100ce0c6d27b01238831b49778acfe80
Isaac Zeng <ndtm.idea@gmail.com>
2018-01-08 10:56:38 +0800

    java 1.9: remove javax.xml.bind imports

--- src/tools/crypto.clj
+++ src/tools/crypto.clj
@@ -3,16 +3,27 @@
   (:require [clojure.java.io :as io])
   (:import [java.security MessageDigest]
            [javax.crypto Mac]
-           [javax.crypto.spec SecretKeySpec]
-           [javax.xml.bind DatatypeConverter]))
+           [javax.crypto.spec SecretKeySpec]))
 
 (defn ->bytes [s]
   (if (bytes? s)
     s
     (.getBytes s "UTF-8")))
 
-(definline hex [^"[B" bs]
-  `(DatatypeConverter/printHexBinary ~bs))
+(def ^"[C" HEX-CODES (.toCharArray "0123456789ABCDEF"))
+
+(defn hex [^"[B" bs]
+  (let [^StringBuilder r (StringBuilder. (* 2 (alength bs)))]
+    (dotimes [i (alength bs)]
+      (let [b (aget bs i)]
+        (->> (bit-shift-right b 4)
+             (bit-and 0xF)
+             ^char (aget HEX-CODES)
+             (.append r))
+        (->> (bit-and b 0xF)
+             ^char (aget HEX-CODES)
+             (.append r))))
+    (.toString r)))
 
 (defn base64 [s]
   (.. java.util.Base64
